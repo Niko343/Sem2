c490b53fe8b360d1a4a8d2f7f5a9e87de2cb15c2
Aleksahin Nikolay <aleksahinn4@gmail.com>
2022-04-24 21:25:56 +0300

    Fawdh

--- Ex1/Program.cs
+++ Ex1/Program.cs
@@ -1,6 +1,17 @@
-﻿Console.WriteLine("Введите трёхзначное число: ");
+﻿// Задача 13: Напишите программу, которая выводит
+//третью цифру заданного числа или сообщает, что третьей
+//цифры нет.
+Console.WriteLine("Введите число: ");
 int num = int.Parse(Console.ReadLine());
-
-
-Console.WriteLine(num / 10 % 10);
-
+if (num < 100)
+{ 
+    Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    while (num > 1000)
+    {
+        num = num / 10;
+    }
+    Console.WriteLine(num % 10);
+}
