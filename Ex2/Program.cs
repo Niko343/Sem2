18c2eccef534043131df995241dec71337b1d9e8
Aleksahin Nikolay <aleksahinn4@gmail.com>
2022-04-24 19:54:45 +0300

    2

---
+++ Ex2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите первое число: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int num2 = int.Parse(Console.ReadLine());
+if (num1 % num2 == 0)
+{
+    Console.WriteLine($"Число {num2} кратно числу {num1}");
+}
+else
+{
+    Console.WriteLine($"Число {num1} делится на {num2} с остатком {num1 % num2}");
+}
+
+
