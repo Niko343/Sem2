1d38ad6cec3755fb1d93ef2d5a2fe2fbfb1a1e19
Aleksahin Nikolay <aleksahinn4@gmail.com>
2022-04-24 20:02:47 +0300

    3

---
+++ Ex3/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+if (num % 7 == 0)
+{
+    if (num % 23 == 0)
+    {
+        Console.WriteLine($"Число {num} делится на 7 и на 23");
+    }
+    else 
+    {
+        Console.WriteLine("Нет");
+    }
+}
+else
+{
+    Console.WriteLine("Нет");
+}
